690b872b7a72919bb28babd3bf3f40a8f8a3a7ae
v-sguo <72851061+v-sguo@users.noreply.github.com>
2020-12-04 14:59:44 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestEnvKeyVault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mySecret = Environment.GetEnvironmentVariable("MySecret");
+            if(mySecret == "IHave3Cats")
+            {
+                Console.WriteLine("Correct Environment Variable");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Environment Variable");
+            }
+        }
+    }
+}
